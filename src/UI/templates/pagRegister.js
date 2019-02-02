59f735b1e37fbd1db77f73982d4fd0920932710a
MirellaLucero <lucero_200900@hotmail.com>
2019-02-02 17:37:50 -0500

    implementando template de register

---
+++ src/UI/templates/pagRegister.js
@@ -0,0 +1,23 @@
+export default () => {
+    const div = document.createElement('div');
+    const divContent = `
+  <h2 class="text-center">únete a nuestra comunidad</h2>
+  <form>
+    <input type="text" id="createUsername" class="username" placeholder="nombre de usuario">
+    <input type="text" id="createEmail"  class="correo" placeholder="correo">
+    <input type="password" id="createPassword" class="password" placeholder="constraseña">
+    <button class="btn-register" id="buttonAcceptRegister">
+    <span>
+    <a href="#/post">
+    Registrar
+    </a>
+    </span>
+    </button>
+  </form>
+</div>`;
+    div.innerHTML = divContent;
+    //capturando acciones del DOM
+    btnRegister = document.querySelector('#buttonAcceptRegister');
+    btnRegister.addEventListener('click', registerOnSubmit);
+    return div;
+}
