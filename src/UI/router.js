84f1a9f7b8643273d669520b310e2c49e6aced49
MirellaLucero <lucero_200900@hotmail.com>
2019-02-02 17:27:45 -0500

    implementando la página de router, importando los templates y poniendo los casos en el viewTemp

--- src/UI/router.js
+++ src/UI/router.js
@@ -1,4 +1,7 @@
 import Inite from '.UI/templates/pagInite.js';
+import Login from '.UI/templates/pagLogin.js';
+import Register from '.UI/templates/pagRegister.js';
+import Home from 'UI/templates/home.js';
 
 const changeTmp = (hash) => {
   if (hash === '#/' || hash === '' || hash === '#') {
@@ -11,10 +14,27 @@ const changeTmp = (hash) => {
 }
 
 const viewTmp = (routers) => {
-  const router = routers.substr(2, routers.length - 2)
-  const container = document.getElementById("container")
-  container.innerHTML = templates[router];
-}
+    const router = routers.substr(2, routers.length - 2)
+    const root = document.getElementById('container');
+    container.innerHTML = '';
+    switch (router) {
+      case 'inite':       
+          container.appendChild(Inite());  
+        break;
+      case 'pagIniteSesion':
+        container.appendChild(Login());
+        break;
+        case 'pagRegister':       
+          container.appendChild(Register());  
+        break;
+        case 'home':       
+          container.appendChild(Home());  
+        break;
+      default:
+        container.appendChild(Inite());
+        break;
+    }
+  }
 
 export const initRouter = () => {
     window.addEventListener('load', changeTmp(window.location.hash))
